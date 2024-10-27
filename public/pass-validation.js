4fe4fcd3a93bc82d25e77852f4e22e5c3c2a453b
bit mechanix llc <enrique.pineda@gmail.com>
2024-10-27 03:25:00 -0400

    reformat

--- public/pass-validation.js
+++ public/pass-validation.js
@@ -1,14 +1,14 @@
-      const startingWith = (subject, replacement) => ({ name, value }) => {
-        if (name.startsWith(subject)) {
-          name = name.replace(subject, replacement);
-        }
+const startingWith = (subject, replacement) => ({ name, value }) => {
+	if (name.startsWith(subject)) {
+		name = name.replace(subject, replacement);
+	}
         return { name, value };
-      };
+}
 
-      document.addEventListener("alpine:init", () => {
-        Alpine.prefix("data-x");
+document.addEventListener("alpine:init", () => {
+	Alpine.prefix("data-x");
         Alpine.mapAttributes(startingWith("data-xon.", Alpine.prefixed("on:")));
         Alpine.mapAttributes(
-          startingWith("data-xbind.", Alpine.prefixed("bind:"))
-        );
-      });
+		startingWith("data-xbind.", Alpine.prefixed("bind:"))
+	)
+})
