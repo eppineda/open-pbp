82618b89d0f8ca89f774195aeacb07e49c4824bf
bit mechanix llc <enrique.pineda@gmail.com>
2024-11-03 22:58:28 -0500

    externalize component init

--- public/chat-util.js
+++ public/chat-util.js
@@ -1,6 +1,8 @@
-function clear() { document.getElementById('sayBox').value = ''; what = '' }
+function clear() { document.getElementById('sayBox').value = ''; this.what = '' }
 function scrollDown() {
 	var content = document.getElementById('textarea');
 	content.scrollTop = content.scrollHeight;
 }
 function updateConversation() { this.conversation += `${ this.who }> ${ this.what }\n` }
+
+export default { clear, scrollDown, updateConversation }
